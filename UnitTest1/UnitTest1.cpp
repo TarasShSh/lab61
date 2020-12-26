491f4303061ec7f5b5fb8692a44ab43287c49a33
Taras Shevchenko <taras.shevchenko.itis.2020@lpnu.ua>
2020-12-26 10:47:10 +0200

    first

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab61/lab61.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int c[27] = { -23, -27, 18, 3, -24, 23, -8, 1, -20, -28, 15, 8, 2, -11, 20, 5, -19, 0, -7, -2, -21, 13, -18, -17, -7, -9, -25 },
+				 sum = -158,
+				 size = 27;
+			Assert::AreEqual(sum, Sum(c, size));
+			Assert::AreEqual(size, Size(c, size));
+		}
+	};
+}
---
+++ lab61/lab61.cpp
@@ -0,0 +1,57 @@
+// lab61.cpp
+// Шевченко, Тарас 
+// Лабораторна робота № 6.1
+// Пошук елементів одновимірного масиву ітераційним та рекурсивним способом
+// Варіант 11
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+void Create(int c[], const int size, const int Low, const int High)
+{
+	for (int i = 0; i < size; i++)
+		c[i] = Low + rand() % (High - Low + 1);
+}
+void Print(int c[], const int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << setw(4) << c[i];
+	cout << endl;
+}
+int Sum(int c[], const int size)
+{
+	int S = 0;
+	for (int i = 0; i < size; i++)
+			S += c[i];
+	return S;
+}
+int Size(int c[], const int size)
+{
+	int length = 0;
+	for (int i = 0; i < size; i++)
+	length += 1;
+	return length;
+}
+void Null(int c[], const int size)
+{
+	for (int i = 0; i < size; i++)
+		if ((c[i] > 0) || !(c[i] % 2 == 0))
+			c[i] = 0;
+}
+int main()
+{
+	srand((unsigned)time(NULL));
+	const int n = 27;
+	int c[n];
+	int Low = -35;
+	int High = 25;
+	Create(c, n, Low, High);
+	Print(c, n);
+	Null(c, n);
+	Print(c, n);
+	cout << "sum = " << Sum(c, n) << endl;
+	cout << "size = " << Size(c, n) << endl;
+	system("pause");
+	return 0;
+}
